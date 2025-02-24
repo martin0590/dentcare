706cbc31bbdbd6d0b1d0acbc492d2ef71916f827
Martin M <martin.ale590@gmail.com>
2025-02-24 12:33:09 -0300

    changed - config

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,6 +1,6 @@
 import StatCard from '@/components/StatCard'
 import { DataTable } from '@/components/table/DataTable'
-import { Payment, columns } from '@/components/table/columns'
+import { columns } from '@/components/table/columns'
 import { getRecentAppointmentList } from '@/lib/actions/appointment.actions'
 import Image from 'next/image'
 import Link from 'next/link'
@@ -35,7 +35,7 @@ const Admin = async () => {
         <section className='admin-stat'>
           <StatCard 
             type="appointments"
-            count={appointments.scheduledCount}
+            count={appointments?.scheduledCount}
             label="Scheduled Appointments"
             icon="/assets/icons/appointments.svg"
           />
--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,8 @@
 const nextConfig = {
   typescript: {
     ignoreBuildErrors: true
-  }
+  },
+  ignoreBuildErrors: true
 };
 
 export default nextConfig;
